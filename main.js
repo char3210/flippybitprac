fc9a486ff5f0d80ef9bfa18a4b8ad0206eed162f
char3210 <45241413+char3210@users.noreply.github.com>
2024-02-11 21:41:33 -0600

    initial

---
+++ main.js
@@ -0,0 +1,96 @@
+"use strict";
+
+let buttons = [false,false,false,false,false,false,false,false]
+
+const keys = ["KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK"]
+
+const input = document.getElementById("input")
+const numbers = document.getElementById("numbers")
+const scoreelem = document.getElementById("score")
+const timeelem = document.getElementById("time")
+
+let score = 0
+let starttime = Date.now()
+
+let buf = []
+restart()
+
+window.addEventListener("keydown", (event) => {
+	if (event.repeat) return
+	let i = keys.indexOf(event.code)
+	if (i == -1) return
+	buttons[i] = !buttons[i]
+	tick()
+})
+
+document.getElementById("reset").onclick = (event) => {
+	restart()
+}
+
+function restart() {
+	starttime = Date.now()
+	score = 0
+	buf = []
+	buf.push(getRandomInt())
+	buf.push(getRandomInt())
+	buf.push(getRandomInt())
+	for (let i = 0; i < 8; i++) {
+		buttons[i] = false
+	}
+	updateScore()
+	updateBuf()
+}
+
+function tick() {
+	if (getInputNum() == buf[buf.length-1]) {
+		for (let i = 0; i < 8; i++) {
+			buttons[i] = false
+		}
+		score++
+		buf.pop()
+		buf.splice(0, 0, getRandomInt())
+		updateBuf()
+		updateScore()
+	}
+
+	input.innerText = buttons.map(a => a ? "O" : "_").join(" ")
+}
+
+function updateBuf() {
+	const list = buf.map(num => {
+		const elem = document.createElement("h2")
+		elem.innerText = getHex(num)
+		return elem
+	})
+	numbers.replaceChildren(...list)
+}
+
+function updateScore() {
+	scoreelem.innerText = "Score: " + score
+}
+
+function getInputNum() {
+	let res = 0
+	for (let i = 0; i < 8; i++) {
+		res <<= 1
+		if (buttons[i]) res++
+	}
+	return res
+}
+
+function getRandomInt() {
+	// return Math.floor(Math.random() * 15)+1
+	let yea = [10,11,12]
+	return yea[Math.floor(Math.random()*yea.length)]
+}
+
+function getHex(val) {
+	const chars = "0123456789ABCDEF"
+	return ""+chars.charAt(val)
+}
+
+setInterval(() => {
+	input.innerText = buttons.map(a => a ? "O" : "_").join(" ")
+	timeelem.innerText = "Time: " + ((Date.now() - starttime)/1000).toFixed(1)
+}, 10)
+
